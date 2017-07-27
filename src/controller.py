4d397ae3775d2187ff126e8659eec44554880d7c
Niels Harder <n.harder@avisi.nl>
2017-07-27 17:47:53 +0200

    Wollah dikke pens

--- src/controller.py
+++ src/controller.py
@@ -1,23 +1,24 @@
 #!/usr/bin/python3
 
 import sys
-import sniffer.py
-import spoofer.py
+import sniffer
+from spoofer import Spoofer
 
-def main(argv):
- Controller(sys.argv[0], sys.argv[1])
-
-if __name__ == "__main__":
- main(sys.argv)
 
 class Controller:
- self.bandwidth_limit
- self.penalty_time
- self.users = []
+    def testSpoofer(self):
+        testspoofer = Spoofer("test")
+        testspoofer.disassociate("test")
 
- def __init__(self, limit, penalty_time):
-  self.bandwidth_limit = limit
-  self.penalty_time = penalty_time
+    def __init__(self, limit, penalty_time):
+        self.bandwidth_limit = limit
+        self.penalty_time = penalty_time
+        self.testSpoofer()
 
 
+def main(argv):
+    Controller(sys.argv[0], sys.argv[1])
 
+
+if __name__ == "__main__":
+    main(sys.argv)
